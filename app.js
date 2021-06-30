616309819045be4d62a8417298bfa04f700bcad4
duhaghazleh <>
2021-06-30 13:32:23 +0000

    anime js file

---
+++ app.js
@@ -0,0 +1,18 @@
+
+alert("Welcome to our Website")
+
+
+var name = prompt("What is your name ?")
+
+console.log(name)
+
+
+var favanime = prompt ("What is your fav anime?")
+
+if ( favanime == "Pokemon")
+{
+  alert("You Entered Pokemon ")
+}
+else{
+  document.write( favanime)
+}
